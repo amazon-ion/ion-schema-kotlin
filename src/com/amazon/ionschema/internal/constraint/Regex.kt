866a64805999d0c3367f8d5869e9be021f94e2d4
Peter Cornell <pcornell@amazon.com>
2019-06-04 17:48:09 -0700

    adds copyright, updates package and imports to c.a.i

--- src/com/amazon/ionschema/internal/constraint/Regex.kt
+++ src/com/amazon/ionschema/internal/constraint/Regex.kt
@@ -1,11 +1,26 @@
-package software.amazon.ionschema.internal.constraint
+/*
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
-import software.amazon.ion.IonString
-import software.amazon.ion.IonText
-import software.amazon.ion.IonValue
-import software.amazon.ionschema.InvalidSchemaException
-import software.amazon.ionschema.Violations
-import software.amazon.ionschema.Violation
+package com.amazon.ionschema.internal.constraint
+
+import com.amazon.ion.IonString
+import com.amazon.ion.IonText
+import com.amazon.ion.IonValue
+import com.amazon.ionschema.InvalidSchemaException
+import com.amazon.ionschema.Violations
+import com.amazon.ionschema.Violation
 import java.util.regex.Pattern
 
 /**
