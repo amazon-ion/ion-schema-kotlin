63aba6694d086c5c0dd43d5de83b66cc54ad3701
Matthew Pope <81593196+popematt@users.noreply.github.com>
2023-05-31 16:01:01 -0700

    Adds readers for schema headers and footers (#271)

---
+++ ion-schema/src/test/kotlin/com/amazon/ionschema/reader/internal/FooterReaderTest.kt
@@ -0,0 +1,62 @@
+package com.amazon.ionschema.reader.internal
+
+import com.amazon.ion.system.IonSystemBuilder
+import com.amazon.ionschema.InvalidSchemaException
+import com.amazon.ionschema.internal.util.IonSchema_2_0
+import com.amazon.ionschema.model.ExperimentalIonSchemaModel
+import com.amazon.ionschema.model.SchemaDocument
+import com.amazon.ionschema.model.UserReservedFields
+import com.amazon.ionschema.util.bagOf
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+@OptIn(ExperimentalIonSchemaModel::class)
+class FooterReaderTest {
+
+    val ION = IonSystemBuilder.standard().build()
+    private val footerReader = FooterReader { it in userReservedFields.footer || !IonSchema_2_0.RESERVED_WORDS_REGEX.matches(it) }
+
+    @Test
+    fun `readFooter can read an empty footer`() {
+        val context = ReaderContext()
+        val footer = footerReader.readFooter(context, ION.singleValue("schema_footer::{}"))
+        assertEquals(SchemaDocument.Item.Footer(), footer)
+    }
+
+    @Test
+    fun `readFooter can read a footer with unreserved open content`() {
+        val context = ReaderContext()
+        val footer = footerReader.readFooter(context, ION.singleValue("schema_footer::{_foo:1,_bar:2}"))
+
+        val expected = SchemaDocument.Item.Footer(
+            openContent = bagOf(
+                "_foo" to ION.newInt(1),
+                "_bar" to ION.newInt(2),
+            )
+        )
+
+        assertEquals(expected, footer)
+    }
+
+    @Test
+    fun `readFooter can read a footer with a user reserved field as open content`() {
+        val context = ReaderContext()
+        context.userReservedFields = UserReservedFields(footer = setOf("foo", "bar"))
+        val footer = footerReader.readFooter(context, ION.singleValue("schema_footer::{foo:1,bar:2}"))
+        val expected = SchemaDocument.Item.Footer(
+            openContent = bagOf(
+                "foo" to ION.newInt(1),
+                "bar" to ION.newInt(2),
+            )
+        )
+
+        assertEquals(expected, footer)
+    }
+
+    @Test
+    fun `readFooter throws exception when illegal field is open content`() {
+        val context = ReaderContext()
+        assertThrows<InvalidSchemaException> { footerReader.readFooter(context, ION.singleValue("schema_footer::{type:1}")) }
+    }
+}
